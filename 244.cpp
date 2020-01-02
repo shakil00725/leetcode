89103048bffc50f58394046a4e01d09b8090bf31
shakil00725 <shakilme0031@gmail.com>
2020-01-02 12:14:42 -0600

    leetcode 244

--- 244.cpp
+++ 244.cpp
@@ -1,1 +1,15 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
 
+bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
+}
+int main()
+{
+    cout<<isAnagram("anagram","nagaram");
+
+}
